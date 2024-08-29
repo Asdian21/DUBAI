ba6d66d458d1992baa135a739d1daff6331f4630
Asdian <asdiank7@gmail.com>
2024-08-29 23:34:27 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,38 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var splide = new Splide(".splide", {
+    perPage: 1,
+    arrows: false,
+  });
+  splide.mount();
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  var splide = new Splide(".splide_2", {
+    perPage: 3,
+    arrows: false,
+  });
+  splide_2.mount();
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  var splide = new Splide(".splide_3", {
+    perPage: 3,
+    arrows: false,
+    breakpoints: {
+      768: {
+        perPage: 2,
+      },
+      480: {
+        perPage: 1,
+      },
+    },
+  });
+  splide_3.mount();
+});
+
+function toggleMenu() {
+  const menu = document.getElementById("menu");
+  const body = document.body;
+  menu.classList.toggle("active");
+  body.classList.toggle("menu-open"); // Переключаем класс 'menu-open' на body
+}
